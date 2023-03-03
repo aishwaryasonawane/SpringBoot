b2c837884cfcef6e4e89a46348c479a368597ff2
aishwaryasonawane <101195799+aishwaryasonawane@users.noreply.github.com>
2023-03-03 23:14:10 +0530

    Add files via upload

---
+++ Anudip_link/src/main/java/com/edu/anudip/AnudipLinkApplication.java
@@ -0,0 +1,13 @@
+package com.edu.anudip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnudipLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnudipLinkApplication.class, args);
+	}
+
+}
---
+++ Anudip_link/src/test/java/com/edu/anudip/AnudipLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.anudip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnudipLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
